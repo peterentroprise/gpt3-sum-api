0b1d1439ab1be4ae5e61560d9d4b6e82cb70f286
Zach Weiss <zach@waterstories.com>
2023-02-18 09:50:21 -0500

    asd

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbed
 from pyannote.audio import Audio
 from pyannote.core import Segment
 from scipy.spatial.distance import cdist
+import moviepy.editor as mp
+import shutil
 
 model = PretrainedSpeakerEmbedding(
     "speechbrain/spkrec-ecapa-voxceleb",
@@ -35,21 +37,24 @@ async def root():
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
-    file_contents = await file.read()
-    temp_file = io.BytesIO(file_contents)
-    temp_file.seek(0)
+
+    with open("tempVideo.webm", "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+    video_clip = mp.VideoFileClip("tempVideo.webm")
+    video_clip.audio.write_audiofile(r"tempAudio.wav")
 
     audio = Audio(sample_rate=16000, mono=True)
 
     speaker1 = Segment(0., 1.)
-    waveform1, sample_rate = audio.crop(temp_file, speaker1)
+    waveform1, sample_rate = audio.crop("tempAudio.wav", speaker1)
     embedding1 = model(waveform1[None])
 
     speaker2 = Segment(1., 2.)
-    waveform2, sample_rate = audio.crop(temp_file, speaker2)
+    waveform2, sample_rate = audio.crop("tempAudio.wav", speaker2)
     embedding2 = model(waveform2[None])
 
     distance = cdist(embedding1, embedding2, metric="cosine")
-    print(distance)
+    distanceNumber = distance[0][0]
 
-    return {"filename": file.filename}
+    return {"filename": file.filename, "distance": distanceNumber}
